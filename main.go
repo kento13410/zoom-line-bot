6958452052b02cdb9e90911a267d5db9a404e108
kento13410 <kento13410@gmail.com>
2024-08-04 14:21:41 +0900

    次回会議昨日追加

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -52,13 +54,36 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					if err != nil {
 						log.Fatal(err)
 					}
-					response, err := zoom.CreateZoomMeeting(token.AccessToken)
+					response, err := zoom.CreateZoomMeeting(token.AccessToken, time.Now())
 					if err != nil {
 						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("zoom meetingの作成に失敗しました: %s", err.Error()))).Do()
 						return
 					}
 
 					bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("zoom meetingの作成に成功しました: %s", response.JoinURL))).Do()
+				} else if match, _ := regexp.MatchString(`次回会議 (\d{1,2}月\d{1,2}日 \d{1,2}:\d{2})`, message.Text); match {
+					re := regexp.MustCompile(`次回会議 (\d{1,2}月\d{1,2}日 \d{1,2}:\d{2})`)
+					match := re.FindStringSubmatch(message.Text)
+					if len(match) > 1 {
+						layout := "2006年1月2日 15:04"
+						fullDate := fmt.Sprintf("%d年%s", time.Now().Year(), match[1])
+						meetingTime, err := time.ParseInLocation(layout, fullDate, time.Local)
+						if err != nil {
+							bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("日付の解析に失敗しました: %s", err.Error()))).Do()
+							return
+						}
+						token, err := zoom.GetAccessToken(os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"), os.Getenv("ACCOUNT_ID"))
+						if err != nil {
+							log.Fatal(err)
+						}
+						response, err := zoom.CreateZoomMeeting(token.AccessToken, meetingTime)
+						if err != nil {
+							bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("zoom meetingの作成に失敗しました: %s", err.Error()))).Do()
+							return
+						}
+
+						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("次回会議の日程は%sです。\nこちらのリンクから参加してください: %s\n本メッセージをアナウンスしてください。", meetingTime.Format("2006年1月2日 15:04"), response.JoinURL))).Do()
+					}
 				}
 			}
 		}
